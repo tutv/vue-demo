35536ca816e7e0ad49519c2994c91e07183671e0
Max <tutv95@gmail.com>
2017-08-02 11:36:53 +0700

    add resource

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 
         <div class="row">
             <div class="col-xs-12">
-                <router-view></router-view>
+                <transition name="slide" mode="out-int">
+                    <router-view></router-view>
+                </transition>
             </div>
         </div>
     </div>
@@ -25,5 +27,36 @@
 <style>
     body {
         padding: 30px;
+    }
+
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+    }
+
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
     }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,11 +22,13 @@
                 <strong class="nav navbar-text navbar-right">Funds: {{ funds | currency}}</strong>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#" @click.prevent="endDay">End day</a></li>
-                    <li class="dropdown">
+                    <li class="dropdown"
+                        @click="isDropdownOpen = !isDropdownOpen"
+                        :class="{open: isDropdownOpen}">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                            aria-expanded="false">Save & load <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save data</a></li>
+                            <li><a href="#" @click.prevent="saveData">Save data</a></li>
                             <li><a href="#">Load data</a></li>
                         </ul>
                     </li>
@@ -41,6 +43,11 @@
 
     export default {
         name: 'app-header',
+        data() {
+            return {
+                isDropdownOpen: false
+            }
+        },
         computed: {
             funds() {
                 return this.$store.getters.funds;
@@ -52,6 +59,15 @@
             ]),
             endDay() {
                 this.randomizeStocks();
+            },
+            saveData() {
+                const data = {
+                    funds: this.$store.getters.funds,
+                    stockPortfolio: this.$store.getters.stockPortfolio,
+                    stocks: this.$store.getters.stocks
+                };
+
+                this.$http.put('data.json', data);
             }
         }
     }
